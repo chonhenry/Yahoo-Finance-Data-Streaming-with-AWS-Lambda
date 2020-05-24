ca0c8d89a95c20b2498d134cea57869d975be3a8
Henry Chon <47991972+chonhenry@users.noreply.github.com>
2020-05-24 17:13:46 -0400

    modified query

--- query.sql
+++ query.sql
@@ -1,14 +1,14 @@
-SELECT t1.company, t1.hour, substring(ts,1,19) as datatime , t1.hourly_high_price
+select t1.company, t1.hour, t2.ts as datetime, t1.hourly_high_price
 
 from (
-    SELECT name as company, substring(ts,12,2) as hour, max(high) as hourly_high_price
-    FROM  "sta-9760-project-3-db"."01"
-    GROUP BY 1, 2
-    ORDER BY 1, 2
+    select name as company, hour(date_add('hour',-4,cast(ts as timestamp))) as hour, max(high) as hourly_high_price
+    from  "sta-9760-project-3-db"."01"
+    group BY 1, 2
+    order BY 1, 2
      ) t1,
   "sta-9760-project-3-db"."01" t2
  
-where t1.company=t2.name and  t1.hour = substring(ts,12,2) and t1.hourly_high_price = t2.high
+where t1.company=t2.name and  t1.hour = hour(date_add('hour',-4,cast(t2.ts as timestamp))) and t1.hourly_high_price = t2.high
  
-order by 1, 2
+order by 1, 3
 ;
